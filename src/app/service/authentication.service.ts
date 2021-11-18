c10c84964b1d9914049a7dcefa450f77f93f0d6c
sourabhpawar <sourabh_pawar@persistent.com>
2021-11-18 16:44:33 +0530

    Second commit

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -12,7 +12,7 @@ export class AuthenticationService {
 
   authenticate(username, password) {
     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return this.httpClient.get<User>('http://localhost:8080/employees/validateLogin',{headers}).pipe(
+    return this.httpClient.get<User>('http://localhost:8080/users/validateLogin',{headers}).pipe(
      map(
        userData => {
         sessionStorage.setItem('username',username);
--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -13,43 +13,66 @@ export class HttpClientService {
   }
 
   getUsers() {
-    let name ='user'
-    let password ='user'
-    const headers = new HttpHeaders({Authorization: 'Basic' + btoa(name + ':' + password)})
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
     return this.httpClient.get<User[]>('http://localhost:8080/users/get',{headers});
   }
 
   addUser(newUser: User) {
-    let name ='user'
-    let password ='user'
-    const headers = new HttpHeaders({Authorization: 'Basic' + btoa(name + ':' + password)})
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
     return this.httpClient.post<User>('http://localhost:8080/users/add', newUser,{headers});
   }
 
   deleteUser(id) {
-    let name ='user'
-    let password ='user'
-    const headers = new HttpHeaders({Authorization: 'Basic' + btoa(name + ':' + password)})
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
     return this.httpClient.delete<User>('http://localhost:8080/users/' + id,{headers});
   }
 
   getBooks() {
-    return this.httpClient.get<Book[]>('http://localhost:8080/books/get');
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    return this.httpClient.get<Book[]>('http://localhost:8080/books/get',{headers});
   }
 
   addUploadData(selectedFile) {
-    return this.httpClient.post('http://localhost:8080/books/upload', selectedFile);
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    return this.httpClient.post('http://localhost:8080/books/upload', selectedFile,{headers});
   }
 
   addBook(newBook) {
-    return this.httpClient.post<Book>('http://localhost:8080/books/add', newBook);
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    return this.httpClient.post<Book>('http://localhost:8080/books/add', newBook,{headers});
   }
 
   deleteBook(id) {
-    return this.httpClient.delete<Book>('http://localhost:8080/books/' + id);
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    return this.httpClient.delete<Book>('http://localhost:8080/books/' + id,{headers});
   }
 
   updateBook(updatedBook: Book) {
-    return this.httpClient.put<Book>('http://localhost:8080/books/update', updatedBook);
+    let username='javainuse'
+    let password='password'
+  
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    return this.httpClient.put<Book>('http://localhost:8080/books/update', updatedBook,{headers});
   }
 }
